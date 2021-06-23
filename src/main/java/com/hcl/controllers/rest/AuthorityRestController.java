9a146f2b3fdb9ddb40bfa82f9df18711bdf16d3d
Joseph Byrd <joseph.byrdjr@hcl.com>
2021-06-23 13:34:55 -0400

    Refactored authorities to be authority

--- src/main/java/com/hcl/controllers/rest/AuthorityRestController.java
+++ src/main/java/com/hcl/controllers/rest/AuthorityRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class AuthoritiesRestController {
+public class AuthorityRestController {
 
     @Autowired
     AuthService authService;
